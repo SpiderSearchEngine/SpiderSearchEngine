2d7a10b6d104f80ba2c27c3cdf6ece912bd3a04e
jairo <jairo@jairo-TOSHIBA-NB505>
2015-05-07 01:28:33 -0600

    Mejora tokenizer

--- src/Logic/tokenizer.java
+++ src/Logic/tokenizer.java
@@ -33,11 +33,26 @@ public class tokenizer <G>{
         StringTokenizer st = new StringTokenizer(dato);
         while (st.hasMoreTokens()) {
             dato = (String)st.nextToken();
-            if (dato.length()>=3 && dato!="que" && dato!="ante" && dato!="bajo"
-                    && dato!="cabe" && dato!="con" && dato!="contra" &&dato!="desde"
-                    && dato!="entre" && dato!="hacia" && dato!="hasta" && dato!="para"
-                    && dato!="por" && dato!="según" && dato!="sin" && dato!="sobre"
-                    && dato!="tras" && dato!="las"  && dato!="los"){
+            if (dato.length()>=3 && dato.compareTo("ante")!=0 
+                    && dato.compareTo("bajo")!=0 && dato.compareTo("cabe")!=0 
+                    && dato.compareTo("con")!=0 && dato.compareTo("contra")!=0 
+                    && dato.compareTo("desde")!=0 && dato.compareTo("entre")!=0 
+                    && dato.compareTo("hacia")!=0 && dato.compareTo("hasta")!=0 
+                    && dato.compareTo("para")!=0 && dato.compareTo("por")!=0 
+                    && dato.compareTo("según")!=0 && dato.compareTo("sin")!=0 
+                    && dato.compareTo("sobre")!=0 && dato.compareTo("tras")!=0 
+                    && dato.compareTo("que")!=0 && dato.compareTo("del")!=0 
+                    && dato.compareTo("los")!=0 && dato.compareTo("las")!=0 
+                    && dato.compareTo("Ante")!=0 && dato.compareTo("Bajo")!=0 
+                    && dato.compareTo("Cabe")!=0 && dato.compareTo("Con")!=0 
+                    && dato.compareTo("Contra")!=0 && dato.compareTo("Desde")!=0 
+                    && dato.compareTo("Entre")!=0 && dato.compareTo("Hacia")!=0 
+                    && dato.compareTo("Hasta")!=0 && dato.compareTo("Para")!=0
+                    && dato.compareTo("Por")!=0 && dato.compareTo("Según")!=0
+                    && dato.compareTo("Sin")!=0 && dato.compareTo("Sobre")!=0 
+                    && dato.compareTo("Tras")!=0 && dato.compareTo("Que")!=0 
+                    && dato.compareTo("Del")!=0 && dato.compareTo("Los")!=0
+                    && dato.compareTo("Las")!=0){
                 sl.push(dato);
             }
         }
